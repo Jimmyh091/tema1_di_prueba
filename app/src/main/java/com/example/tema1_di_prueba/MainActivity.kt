201dec8fd89da0374502eb44cdd37628b126123b
Jimmyh091 <jaimecaballero164@gmail.com>
2024-09-16 18:17:17 +0200

    commit inicial

--- app/src/main/java/com/example/tema1_di_prueba/MainActivity.kt
+++ app/src/main/java/com/example/tema1_di_prueba/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.tema1_di_prueba
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,6 +11,24 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+
+        val texto = findViewById<TextView>(R.id.textView)
+
+        val boton1 = findViewById<Button>(R.id.button2)
+        val boton2 = findViewById<Button>(R.id.button3)
+
+        boton1.setOnClickListener{
+            texto.text = boton1.text
+        }
+
+        boton2.setOnClickListener{
+            texto.text = boton2.text
+        }
+
+        /*
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -16,5 +36,7 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        */
+
     }
 }
